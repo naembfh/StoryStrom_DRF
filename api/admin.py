ab87267febd9adae121b5e34314fc6a43b652b21
Mehedi Nayem <naembfh@gmail.com>
2024-09-09 00:00:58 +0800

    Category model created

--- api/admin.py
+++ api/admin.py
@@ -5,3 +5,4 @@ from api import models as api_models
 
 admin.site.register(api_models.User)
 admin.site.register(api_models.Profile)
+admin.site.register(api_models.Category)
--- api/models.py
+++ api/models.py
@@ -65,3 +65,20 @@ def save_user_profile(sender, instance, **kwargs):
 
 post_save.connect(create_user_profile, sender=User)
 post_save.connect(save_user_profile, sender=User)
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="image", null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Category"
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+    
