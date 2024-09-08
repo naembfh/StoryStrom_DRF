4c9abf8842e1dcb49b4eaec94c5f5a9e2dc3a7c2
Mehedi Nayem <naembfh@gmail.com>
2024-09-09 03:31:37 +0800

    implement jwt token serializers view and url for create user

---
+++ api/serializer.py
@@ -0,0 +1,74 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from api import models as api_models
+
+# Define a custom serializer that inherits from TokenObtainPairSerializer
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    # Define a custom method to get the token for a user
+    def get_token(cls, user):
+        # Call the parent class's get_token method
+        token = super().get_token(user)
+
+        # Add custom claims to the token
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        try:
+            token['vendor_id'] = user.vendor.id
+        except:
+            token['vendor_id'] = 0
+
+        return token
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    # Define fields for the serializer, including password and password2
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+       
+        model = api_models.User
+       
+        fields = ('full_name', 'email',  'password', 'password2')
+
+    def validate(self, attrs):
+      
+        if attrs['password'] != attrs['password2']:
+            # Raise a validation error if the passwords don't match
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        # Return the validated attributes
+        return attrs
+
+    def create(self, validated_data):
+       
+        user = api_models.User.objects.create(
+            full_name=validated_data['full_name'],
+            email=validated_data['email'],
+        )
+        email_username, mobile = user.email.split('@')
+        user.username = email_username
+
+        # Set the user's password based on the validated data
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+    
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = api_models.User
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = api_models.Profile
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+# Others
+import json
+import random
+
+# Custom Imports
+from api import serializer as api_serializer
+from api import models as api_models
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    # Here, it specifies the serializer class to be used with this view.
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    # It sets the queryset for this view to retrieve all User objects.
+    queryset = api_models.User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.RegisterSerializer
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = api_serializer.ProfileSerializer
+
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+
+        user = api_models.User.objects.get(id=user_id)
+        profile = api_models.Profile.objects.get(user=user)
+        return profile
+    
+
+def generate_numeric_otp(length=7):
+        # Generate a random 7-digit OTP
+        otp = ''.join([str(random.randint(0, 9)) for _ in range(length)])
+        return otp
